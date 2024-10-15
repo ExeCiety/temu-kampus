fa347bf695ba5089ea9b97be49fd3bae038e5902
ExeCiety <sultan.majid213@gmail.com>
2024-10-16 01:21:08 +0700

    🔧 fix send email reminders

--- scripts/send-event-reminders.ts
+++ scripts/send-event-reminders.ts
@@ -2,6 +2,6 @@ import cron from 'node-cron'
 import { sendEmailEventConfirmationJob } from '@/lib/jobs'
 
 // Schedule the task to run daily at 9 AM
-cron.schedule('19 01 * * *', async () => {
+cron.schedule('00 08 * * *', async () => {
   await sendEmailEventConfirmationJob()
 })
